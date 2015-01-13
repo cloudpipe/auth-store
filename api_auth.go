ab7d41a507c2cd4fa2000347e9b6563b2335faa4
Ash Wilson <smashwilson@gmail.com>
2015-01-13 14:42:13 -0500

    Serve a simple string on /v1/style.

---
+++ api_auth.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// StyleHandler reports this auth backend's "style" attribute. This is reported through cloudpipe
+// to API consumers to provide them with a hint about other auth interactions that are possible at
+// this endpoint.
+func StyleHandler(c *Context, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "auth-store")
+}
