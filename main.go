a6c2bd5650279a3b89b76ce5e3807ab132390af5
Ash Wilson <smashwilson@gmail.com>
2015-01-13 14:32:59 -0500

    Main function with context binding.

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+)
+
 func main() {
-	// Stub.
+	c, err := NewContext()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Unable to load application context.")
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"address": c.ListenAddr(),
+	}).Info("Auth API listening.")
+
+	http.ListenAndServeTLS(c.ListenAddr(), c.Cert, c.Key, nil)
+}
+
+// ContextHandler is an HTTP HandlerFunc that accepts an additional parameter containing the
+// server context.
+type ContextHandler func(c *Context, w http.ResponseWriter, r *http.Request)
+
+// BindContext returns an http.HandlerFunc that binds a ContextHandler to a specific Context.
+func BindContext(c *Context, handler ContextHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) { handler(c, w, r) }
 }
