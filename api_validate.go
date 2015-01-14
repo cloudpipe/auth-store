a43fbc7b1008eb67c93dec890b518d3c99edc282
Ash Wilson <smashwilson@gmail.com>
2015-01-14 10:37:52 -0500

    Stub the validate handler.

---
+++ api_validate.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+// ValidateHandler determines whether or not an API key is valid for a specific account.
+func ValidateHandler(c *Context, w http.ResponseWriter, r *http.Request) {
+	if !MethodOk(w, r, "GET") {
+		return
+	}
+}
