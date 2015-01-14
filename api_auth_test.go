fa268accda3035bdc3321d168094454b2ea664a4
Ash Wilson <smashwilson@gmail.com>
2015-01-14 09:30:35 -0500

    Unit tests for account creation.

---
+++ api_auth_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type AuthTestStorage struct {
+	NullStorage
+
+	Created *Account
+}
+
+func (storage *AuthTestStorage) CreateAccount(account *Account) error {
+	storage.Created = account
+	return nil
+}
+
+func TestCreateHandlerSuccess(t *testing.T) {
+	r := HTTPRequest(t, "POST", "https://localhost/v1/accounts", `
+	{
+		"name": "someone@gmail.com",
+		"password": "secret"
+	}
+	`)
+	w := httptest.NewRecorder()
+	s := &AuthTestStorage{}
+	c := &Context{Storage: s}
+
+	CreateHandler(c, w, r)
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Expected 201 response, but got [%d]", w.Code)
+	}
+
+	if w.Body.Len() != 0 {
+		t.Errorf("Expected empty body, but got:<<<\n%s>>>", w.Body.String())
+	}
+
+	acct := s.Created
+
+	if acct == nil {
+		t.Fatal("Account not created")
+	}
+
+	if acct.Name != "someone@gmail.com" {
+		t.Errorf("Account had unexpected name: [%s]", acct.Name)
+	}
+}
+
+func TestCreateHandlerFailure(t *testing.T) {
+	t.Fatal("pending")
+}
