b0ba096dc287ab6ffb6f5accedb51f99da5e8189
Ash Wilson <smashwilson@gmail.com>
2015-01-14 13:52:41 -0500

    Key validation handler.

--- api_validate.go
+++ api_validate.go
@@ -1,10 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
 
 // ValidateHandler determines whether or not an API key is valid for a specific account.
 func ValidateHandler(c *Context, w http.ResponseWriter, r *http.Request) {
 	if !MethodOk(w, r, "GET") {
 		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		APIError{
+			Message: fmt.Sprintf("Unable to parse URL parameters: %v", err),
+		}.Log("").Report(w, http.StatusBadRequest)
+		return
+	}
+
+	accountName, apiKey := r.FormValue("accountName"), r.FormValue("apiKey")
+	if accountName == "" || apiKey == "" {
+		APIError{
+			UserMessage: `Missing required query parameters "accountName" and "apiKey".`,
+			LogMessage:  "Key validation request missing required query parameters.",
+		}.Log("").Report(w, http.StatusBadRequest)
+		return
+	}
+
+	ok, err := c.Storage.AccountHasKey(accountName, apiKey)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			APIError{
+				Message: "Unrecognized account or API key.",
+			}.Log(accountName).Report(w, http.StatusUnauthorized)
+			return
+		}
+		APIError{
+			UserMessage: "Internal storage error encountered. Please try again later.",
+			LogMessage:  fmt.Sprintf("Storage error: %v", err),
+		}.Log(accountName).Report(w, http.StatusInternalServerError)
+		return
+	}
+
+	if ok {
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
--- api_validate_test.go
+++ api_validate_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type ValidateTestStorage struct {
+	NullStorage
+
+	Accept bool
+}
+
+func (storage *ValidateTestStorage) AccountHasKey(name, key string) (bool, error) {
+	return storage.Accept, nil
+}
 
 func TestValidateHandlerSuccess(t *testing.T) {
-	t.Skipf("pending")
+	r := HTTPRequest(t, "GET", "https://localhost/v1/validate?accountName=someone&apiKey=ff01ab", "")
+	w := httptest.NewRecorder()
+	s := &ValidateTestStorage{Accept: true}
+	c := &Context{Storage: s}
+
+	ValidateHandler(c, w, r)
+
+	if w.Code != http.StatusNoContent {
+		t.Errorf("Expected response code %d, but was %d", http.StatusNoContent, w.Code)
+	}
+}
+
+func TestValidateHandlerReject(t *testing.T) {
+	r := HTTPRequest(t, "GET", "https://localhost/v1/validate?accountName=someone&apiKey=ff01ab", "")
+	w := httptest.NewRecorder()
+	s := &ValidateTestStorage{Accept: false}
+	c := &Context{Storage: s}
+
+	ValidateHandler(c, w, r)
+
+	if w.Code != http.StatusNotFound {
+		t.Errorf("Expected response code %d, but was %d", http.StatusNotFound, w.Code)
+	}
 }
